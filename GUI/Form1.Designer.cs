bd2f31234b61f67c3d199f4437671829a039e6fc
Simpan1001 <simon.springer@edu.huddinge.se>
2019-09-24 13:28:21 +0200

    Add Customer function improved and added clear function

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -38,11 +38,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(322, 73);
+            this.button1.Location = new System.Drawing.Point(322, 162);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 23);
             this.button1.TabIndex = 0;
@@ -52,14 +60,14 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(37, 75);
+            this.textBox1.Location = new System.Drawing.Point(37, 126);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(116, 20);
             this.textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(182, 75);
+            this.textBox2.Location = new System.Drawing.Point(182, 126);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(116, 20);
             this.textBox2.TabIndex = 2;
@@ -67,14 +75,14 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(37, 134);
+            this.listBox1.Location = new System.Drawing.Point(37, 257);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(261, 95);
+            this.listBox1.Size = new System.Drawing.Size(261, 134);
             this.listBox1.TabIndex = 3;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(322, 134);
+            this.button2.Location = new System.Drawing.Point(322, 257);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(121, 23);
             this.button2.TabIndex = 4;
@@ -84,7 +92,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(322, 24);
+            this.button3.Location = new System.Drawing.Point(322, 62);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(121, 23);
             this.button3.TabIndex = 5;
@@ -95,7 +103,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(34, 59);
+            this.label1.Location = new System.Drawing.Point(34, 110);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 6;
@@ -104,7 +112,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(179, 59);
+            this.label2.Location = new System.Drawing.Point(179, 110);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(56, 13);
             this.label2.TabIndex = 7;
@@ -112,7 +120,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(37, 24);
+            this.textBox3.Location = new System.Drawing.Point(38, 62);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(261, 20);
             this.textBox3.TabIndex = 8;
@@ -120,17 +128,94 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(34, 9);
+            this.label3.Location = new System.Drawing.Point(34, 46);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.Size = new System.Drawing.Size(41, 13);
             this.label3.TabIndex = 9;
             this.label3.Text = "Search";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(37, 165);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(261, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(37, 204);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(173, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(233, 206);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Phonenumber";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(254, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Favorite";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(322, 312);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Reset";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(167, 18);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(148, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "CUSTOMER MANAGEMENT";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(473, 254);
+            this.ClientSize = new System.Drawing.Size(490, 420);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label2);
@@ -160,6 +245,14 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -17,11 +17,13 @@ namespace GUI
         public Form1()
         {
             InitializeComponent();
-            
+
             //Pre-Generated objects in the list
             namnLista.Add(new Customer() { firstName = "Thor", lastName = "Odinsson", email = "thor.odinsson@vallhalla.com", phoneNumber = "237-740 50 23", favorite = true });
             namnLista.Add(new Customer() { firstName = "Bruce", lastName = "Banner", email = "bruce.banner@smash.com", phoneNumber = "123-456 78 90", favorite = true });
             namnLista.Add(new Customer() { firstName = "Spongebob", lastName = "Squarepants", email = "spongebob.squarepants@spongemail.wc", phoneNumber = "345-038 82 24", favorite = false });
+
+
 
             //Puts together all First and last names into a whole name
             foreach (Customer e in namnLista)
@@ -30,20 +32,30 @@ namespace GUI
             }
         }
 
-        //This method adds names to the list \/
+        //This method adds names and properties to the list \/
         private void Button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text != "" && textBox2.Text != "")
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox4.Text == "" || textBox5.Text == "")
             {
-                Customer c = new Customer() { firstName = textBox1.Text, lastName = textBox2.Text };
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Please add a full description.");
+            }
+            else
+            {
+                Customer c = new Customer() { firstName = textBox1.Text, lastName = textBox2.Text, email = textBox4.Text, phoneNumber = textBox5.Text, favorite = checkBox1.Checked };
                 c.wholeName = (c.firstName + " " + c.lastName);
+                listBox1.Items.Clear();
                 namnLista.Add(c);
                 textBox1.Clear();
                 textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                checkBox1.Checked = false;
+                listBox1.Items.Add("Customer added");
             }
         }
-
-        //This method adds names to the list /\
+        //This method adds names and properties to the list /\
 
         //This method displays all the names in the list onto the listBox \/
         private void Button2_Click(object sender, EventArgs e)
@@ -82,5 +94,27 @@ namespace GUI
             }
         }
         //This method compares the inputed name to all the names in the list. If its a match, then print the info about the name /\
+
+        //This method clears all textspaces and prints out some instructions for the GUI-user\/
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            listBox1.Items.Clear();
+            checkBox1.Checked = false;
+
+            listBox1.Items.Add("1.You can search for a customer in the upper box.");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("2.You can add a new customer by entering the");
+            listBox1.Items.Add("customers description in the boxes above and press ");
+            listBox1.Items.Add("'Add Customer'.");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("3.You can see all customers listed by pressing");
+            listBox1.Items.Add("'Show Customers'");
+        }
+        //This method clears all textspaces and prints out some instructions for the GUI-user\/
     }
 }
